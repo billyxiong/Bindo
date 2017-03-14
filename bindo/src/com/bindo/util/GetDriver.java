b843feeed1bb84cfbc31e0a62f3689f41d792844
Administrator <Administrator@PC201701031452>
2017-03-14 20:41:26 +0800

    okay

--- bindo/src/com/bindo/util/GetDriver.java
+++ bindo/src/com/bindo/util/GetDriver.java
@@ -9,30 +9,42 @@ public class GetDriver {
 	public static WebDriver driver;
 	//获取远程节点中IE浏览器对象Driver，通过函数参数remoteNodeUrl连接不同的指定节点
 	public static String remoteNodeUrl="http://192.168.1.233:5555/wd/hub";
-	public static WebDriver getRemoteIEDriver() throws 
-	MalformedURLException{
+	public static WebDriver getRemoteIEDriver(){
 		DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
 		capability.setBrowserName("internetExplorer");
 		capability.setPlatform(Platform.XP);
-		driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		try {
+			driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return driver;
 	}
 	//获取远程节点机器中的Chrome浏览器对象Driver,通过函数参数remoteNodeUrl连接不同节点
-	public static WebDriver getRemoteChromedriver() throws 
-	MalformedURLException{
+	public static WebDriver getRemoteChromedriver() {
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		capability.setBrowserName("chrome");
 		capability.setPlatform(Platform.XP);
-		driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		try {
+			driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return driver;		
 	}
 	//获取远程节点机器中的firefox浏览器对象Driver,通过函数参数remoteNodeUrl连接不同节点
-	public static WebDriver getRemoteFirefoxdriver() throws 
-	MalformedURLException{
+	public static WebDriver getRemoteFirefoxdriver() {
 		DesiredCapabilities capability = DesiredCapabilities.firefox();
 		capability.setBrowserName("firefox");
 		capability.setPlatform(Platform.XP);
-		driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		try {
+			driver = new RemoteWebDriver(new URL(remoteNodeUrl),capability);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return driver;			
 	}	
 }
